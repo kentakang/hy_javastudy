ef377e3a726158ecce5827b759daacfc355027da
Chan Kang <kentakang@icloud.com>
2018-01-05 02:12:55 +0900

    Add Test5.java
    정수형 배열 예제

---
+++ src/test1/Test5.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Test5 {
+    public static void main(String[] args) {
+        // int[] a = new int[5];
+        int[] a = new int[] {1, 2, 3, 4, 5};
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + " ");
+        }
+    }
+}

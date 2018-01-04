cda8935b0b5c6c3fd8b32b4f249465339f9db1da
Chan Kang <kentakang@icloud.com>
2018-01-05 02:24:31 +0900

    Add Test7.java
    Scanner 클래스 기본 사용법

---
+++ src/test1/Test7.java
@@ -0,0 +1,11 @@
+package test1;
+
+import java.util.Scanner;
+
+public class Test7 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println(a);
+    }
+}

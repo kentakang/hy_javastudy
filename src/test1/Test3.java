dd9c1dc7d6c15cf7334d3562e00d2440b7a89464
Chan Kang <kentakang@icloud.com>
2018-01-05 01:51:43 +0900

    Add Test3.java

---
+++ src/test1/Test3.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Chan c1 = new Chan();
+
+        c1.setHeight(171);
+        c1.setWeight(50);
+
+        System.out.println("키 : " + c1.getHeight() + " 몸무게 : " + c1.getWeight());
+    }
+}

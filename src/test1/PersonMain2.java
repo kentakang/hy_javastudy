d5240b3bd0bf2646b0017e11b5488d80d99877a3
Chan Kang <kentakang@icloud.com>
2018-01-05 02:12:41 +0900

    Modify PersonMain2.java

--- src/test1/PersonMain2.java
+++ src/test1/PersonMain2.java
@@ -4,11 +4,11 @@ public class PersonMain2 {
     public static void main(String[] args) {
         Person[] p = new Person[9];
 
-        for(int i = 0; i < 9; i++) {
+        for(int i = 0; i < p.length; i++) {
             p[i] = new Person("트와이스 " + (i + 1) + "번째 멤버");
         }
 
-        for(int i = 0; i < 9; i++) {
+        for(int i = 0; i < p.length; i++) {
             System.out.println("이름 : " + p[i].getName() + " 나이 : " + p[i].getAge());
         }
     }

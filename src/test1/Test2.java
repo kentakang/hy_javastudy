02f9164798b13864e0c999bf352e69ea2ce2d30f
Chan Kang <kentakang@icloud.com>
2018-01-05 00:00:11 +0900

    Add Test2.java

---
+++ src/test1/Test2.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class Test2 {
+    public static void main(String[] args) {
+        // 반복문을 통해 1 부터 10까지 출력 (한 줄에)
+        for(int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}

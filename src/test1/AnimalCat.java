bd749fb247d78e4413793a34f9cf76cf327f7965
Chan Kang <kentakang@icloud.com>
2018-01-07 13:59:26 +0900

    Add AnimalCat.java

---
+++ src/test1/AnimalCat.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class AnimalCat extends Animal {
+    public void bark() {
+        System.out.println("냥");
+    }
+
+    public int HundredRun() {
+        return 4;
+    }
+}

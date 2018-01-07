b5cacd8d951f9c9aab867eaee7a8b02f83165339
Chan Kang <kentakang@icloud.com>
2018-01-07 13:59:33 +0900

    Add AnimalUnicorn.java

---
+++ src/test1/AnimalUnicorn.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class AnimalUnicorn extends Animal {
+    public void bark() {
+        System.out.println("기리리리ㅣ기기기");
+    }
+
+    public int HundredRun() {
+        return 1;
+    }
+}

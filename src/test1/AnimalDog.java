91e616bc35486681011a3a76f1cfa9911bec9444
Chan Kang <kentakang@icloud.com>
2018-01-07 13:59:18 +0900

    Add AnimalDog.java

---
+++ src/test1/AnimalDog.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class AnimalDog extends Animal {
+
+    public void bark() {
+        System.out.println("멍");
+    }
+
+    public int HundredRun() {
+        return 5;
+    }
+}
